1e4dd5c6e2a92fff17806fd6ab0ab7e9125db745
curogihu <curoasg@gmail.com>
2018-08-01 11:40:00 +0900

    Propsを使う前の準備

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React app</h1>
+        <p>This is really working</p>
+        <Person name="max" age="11" />
+        <Person name="hikozuma" age="22">My Hobbies: Walking</Person>
+        <Person name="aaa" age="33" />
       </div>
     );
   }
