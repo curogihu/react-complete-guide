03c5f7a2177e6efa03ad9f472c4590fa0d2a7a8c
curogihu <curoasg@gmail.com>
2018-08-01 14:14:13 +0900

    イベントハンドラーに値を移す方法2
    ただし、パフォーマンスは良くないそうなので、bind()を用いたほうがいいらしい。

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working</p>
-        <button onClick={ this.switchNameHandler.bind(this, 'abcdefg') }>Switch Name</button>
+        <button onClick={() => this.switchNameHandler('abcdefg') }>Switch Name</button>
         <Person
           name={ this.state.persons[0].name }
           age={ this.state.persons[0].age } />
